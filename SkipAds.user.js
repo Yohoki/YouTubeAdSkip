6dee7f4a93d7b1ab7a205b0821477eeed5d66b12
Yohoki <44595371+Yohoki@users.noreply.github.com>
2023-10-27 02:42:28 -0500

    Create SkipAds.user.js
    Version 1.0.0

---
+++ SkipAds.user.js
@@ -0,0 +1,86 @@
+// ==UserScript==
+// @name         YouTube Ad-Skip
+// @icon         https://www.gstatic.com/youtube/img/branding/favicon/favicon_192x192.png
+// @version      1.0.0
+// @description  Simple ad-blocker bypass. Clicks "Skip Ads" button as soon as possible without triggering the adblock ban.
+// @author       Yohoki
+// @match        https://www.youtube.com/*
+// @match        https://www.youtu.be/*
+// @license      GNU GPL 3.0
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    let State = "Listening";
+    let BlockedInterval = 0;
+    let currentPage = window.location.href;
+
+    // Function to handle the URL change
+    function handleURLChange() {
+        const newURL = window.location.href;
+        if (newURL !== currentPage) {
+            // URL has changed, reset the State
+            State = "Listening";
+            BlockedInterval = 0;
+            setColor();
+            currentPage = newURL;
+        }
+        if (!window.location.href.includes("watch")) {
+            State = "LOCKED";
+            //console.debug("LOCKED");
+        }
+    }
+
+    // Create a function to handle the changes in the DOM
+    function handleDOMChanges(mutationsList) {
+        if (State === "LOCKED") {return;}
+        //Dec color timer and set colors.
+        BlockedInterval--;
+        setColor();
+        //console.debug("blockedinterval: " + BlockedInterval);
+
+        //console.log("AdSkip Observer Listening");
+        State = BlockedInterval > 0 ? State : "Listening";
+        for (let mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                // Check if an ad is showing
+                const adShowing = document.querySelector('.ad-showing');
+                if (adShowing) {
+                    //console.log("Ad is currently Playing.");
+                    State = "Running";
+                    const skipButton = document.querySelector('button.ytp-ad-skip-button.ytp-button');
+                    if (skipButton) {
+                        skipButton.click();
+                        //console.log("Button found and clicked.");
+                        State = "Success";
+                        BlockedInterval = 20;
+                    } else {
+                        //console.log("Skip button not ready.");
+                    }
+                }
+            }
+        }
+    }
+
+    // Set Menu Bar color for status reporting.
+    function setColor() {
+        const menuBar = document.getElementById('guide-icon');
+        menuBar.style.backgroundColor = State === "Listening" ? '#FF0000' : State === "Success" ? '#00FF00' : '#FFFF00';
+    }
+
+    // Create a MutationObserver to watch for changes in the DOM
+    const observer = new MutationObserver(handleDOMChanges);
+
+    // Define the options for the observer
+    const observerOptions = {
+        childList: true, // Watch for changes to the child elements of the target
+        subtree: true, // Watch for changes in the entire subtree
+    };
+
+    // Start observing the DOM
+    observer.observe(document.body, observerOptions);
+
+    // Add an interval to check for URL changes
+    setInterval(handleURLChange, 1000);
+})();
