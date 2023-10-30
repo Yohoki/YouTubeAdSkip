b133125d3172e3d850cdce4b7ec487971924df9a
Yohoki <44595371+Yohoki@users.noreply.github.com>
2023-10-30 01:18:53 -0500

    Update SkipAds.user.js
    Minor tweaks to fix homepage blocks not working.
    
    bugfix: Hopefully fixed status indicator getting stuck if video ad was not able to be skipped.

--- SkipAds.user.js
+++ SkipAds.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube Ad-Skip
 // @icon         https://www.gstatic.com/youtube/img/branding/favicon/favicon_192x192.png
-// @version      1.0.4
+// @version      1.0.5
 // @homepage     https://github.com/Yohoki/YouTubeAdSkip
 // @downloadURL  https://github.com/Yohoki/YouTubeAdSkip/raw/main/SkipAds.user.js
 // @updateURL    https://github.com/Yohoki/YouTubeAdSkip/raw/main/SkipAds.user.js
@@ -14,6 +14,7 @@
 
 (function () {
     'use strict';
+
     // Initialization
 	let State = "Listening";
     let BlockedInterval = 0;
@@ -21,54 +22,75 @@
 
     // Function to handle the changes in the DOM
     function handleDOMChanges(mutationsList) {
-        console.info("AdSkip: Observer Listening");
+        const curTime = new Date(Date.now()).toLocaleTimeString('en-US');
+        //console.debug(curTime + " - AdSkip: Observer Listening");
         for (let mutation of mutationsList) {
             if (mutation.type === 'childList') {
 
                 // Check if an ad is showing
                 const midVidAd = document.querySelector('.ad-showing');
 				const homePageIframe = document.getElementById('player');
-                const homePagePremBanner = document.getElementById('masthead-ad');
-				const homePageInFeed = document.querySelector('ytd-ad-slot-renderer').parentElement;//.parentElement;
+                //const homePagePremBanner = document.getElementById('masthead-ad');
+                const homePagePremBanner = document.querySelectorAll('[id=masthead-ad]');
+				const homePageInFeed = document.querySelectorAll('ytd-ad-slot-renderer');//.parentElement;
+                //console.debug(homePageInFeed);
+                if (!midVidAd && !homePageIframe && !homePagePremBanner && !homePageInFeed) {
+                    State = "Listening";
+                    return;
+                }
 
                 if (midVidAd) { // Mid-video ad break //
-                    //console.log("AdSkip: An ad is currently Playing.");
-                    State = "Running";
+                    console.log(curTime + " - AdSkip: An ad is currently Playing.");
                     const skipButton = document.querySelector('button.ytp-ad-skip-button.ytp-button');
                     if (skipButton) {
                         skipButton.click();
-                        //console.log("AdSkip: Button found and clicked.");
+                        console.log(curTime + " - AdSkip: Button found and clicked.");
+                        State = "Listening"
                         BlockedInterval = 10;
+                        setColor();
                     } else {
+                        State = "Running";
                         BlockedInterval = 0;
                         setColor();
-                        //console.log("AdSkip: Skip button not ready.");
+                        //console.log(curTime + " - AdSkip: Skip button not ready.");
                     }
                 }
-		if (homePageIframe) { // Hopepage Video ad inside iframe //
-			const VideoAd = homePageIframe.contentDocument.querySelector('#movie_player');
-			if (VideoAd.classList.contains("playing-mode")) {
-				VideoAd.classList.remove("playing-mode");
-				VideoAd.classList.add("ytp-small-mode");
-				VideoAd.classList.add("unstarted-mode");
-				console.log("AdSkip: Home Page video ad paused.");
-				BlockedInterval = 10;
-			}
-		}
-		if (homePagePremBanner) { // Homepage, Large banner ad for premium subscription //
+				if (homePageIframe) { // Hopepage Video ad inside iframe //
+					const VideoAd = homePageIframe.contentDocument?.querySelector('#movie_player');
+				    if (VideoAd && VideoAd.classList.contains("playing-mode")) {
+						VideoAd.classList.remove("playing-mode");
+						VideoAd.classList.add("ytp-small-mode");
+						VideoAd.classList.add("unstarted-mode");
+                        console.log(curTime + " - AdSkip: Home Page video ad paused.");
+						BlockedInterval = 10;
+					}
+				}
+				/*if (homePagePremBanner) { // Homepage, Large banner ad for premium subscription //
                     if (homePagePremBanner.style.display != 'none') {
                         homePagePremBanner.style.display = "none";
-                        console.log("AdSkip: Home Page banner hidden.");
+                        console.log(curTime + " - AdSkip: Home Page banner hidden.");
                         BlockedInterval = 10;
                     }
-		}
-		if (homePageInFeed) { // Homepage, Small in-feed ads that look like a video. //
-                    if (homePageInFeed.style.display != 'none') {
-                        homePageInFeed.style.display = 'none';
-                        console.log("AdSkip: Home Page in-feed ad hidden.");
-                        BlockedInterval = 10;
-                    }
-		}
+				}*/
+                if (homePagePremBanner.length>0) {
+                    homePagePremBanner.forEach((banner) => {
+                        if (banner.style.display != 'none') {
+                            banner.style.display = 'none';
+                            console.log(curTime + " - AdSkip: Home Page banner hidden.");
+                            BlockedInterval = 10;
+                        }
+                    });
+                }
+                if (homePageInFeed.length>0) {
+				    homePageInFeed.forEach((temp) => { // Homepage, Small in-feed ads that look like a video. //
+                        const inFeed = temp.parentElement.parentElement;
+                        if (inFeed.style.display != 'none') {
+                            inFeed.style.display = 'none';
+                            console.log(curTime + " - AdSkip: Home Page in-feed ad hidden.");
+                            BlockedInterval = 10;
+                        }
+                    });
+                }
             }
         }
     }
@@ -77,9 +99,11 @@
     function setColor() {
         const menuBar = document.getElementById('guide-icon');
         if (BlockedInterval>0) {
-            State = BlockedInterval == 1 ? "Listening" : "Success";
+            menuBar.style.color = '#00FF00';
+            menuBar.title = "AdSkip: Ad successfully skipped.";
             BlockedInterval--;
-            console.debug("blockedinterval: " + BlockedInterval);
+            return;
+            //console.debug("blockedinterval: " + BlockedInterval);
         }
         switch (State) {
             case "Listening":
@@ -90,10 +114,8 @@
                 menuBar.style.color = '#FFFF00';
                 menuBar.title = "AdSkip: Ad Detected. Trying to skip.";
                 break;
-            case "Success":
-                menuBar.style.color = '#00FF00';
-                menuBar.title = "AdSkip: Ad successfully skipped.";
-                break;
+            /* case "Success":
+                break; */
             default:
                 menuBar.style.color = '#666666';
                 menuBar.title = "AdSkip: Not running on current page. Sum Ting Wong";
