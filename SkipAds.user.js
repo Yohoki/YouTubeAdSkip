b4665c433e702810e10898f97bbbcb07bcbab661
Yohoki <44595371+Yohoki@users.noreply.github.com>
2023-11-03 08:52:19 -0500

    Update SkipAds.user.js
    Refactored code a bit.
    - Added hideElement(), clickElement() and DEBUG_highlightElement() functions to standardize handling of different ad types.
    
    Changed how some ads got blocked. Mostly working now.
    
    Added a debug function to searchbar. Type a label (eg. div), id (eg. #id) or class (eg. .Style) into the searchbar and hit the debug button. This feature is off by default.
    Turn it on in the code by setting `const debugMode = true;`
    
    Example use:
    "yt-icon" will highlight all icons on page.
    "yt-icon#logo-icon" will highlight only the youtube logo.

--- SkipAds.user.js
+++ SkipAds.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube Ad-Skip
 // @icon         https://www.gstatic.com/youtube/img/branding/favicon/favicon_192x192.png
-// @version      1.0.5
+// @version      1.0.6
 // @homepage     https://github.com/Yohoki/YouTubeAdSkip
 // @downloadURL  https://github.com/Yohoki/YouTubeAdSkip/raw/main/SkipAds.user.js
 // @updateURL    https://github.com/Yohoki/YouTubeAdSkip/raw/main/SkipAds.user.js
@@ -14,40 +14,38 @@
 
 (function () {
     'use strict';
+    //Debug mode
+    const debugMode = false;
 
     // Initialization
-	let State = "Listening";
+    let State = "Listening";
     let BlockedInterval = 0;
     setColor();
 
     // Function to handle the changes in the DOM
     function handleDOMChanges(mutationsList) {
         const curTime = new Date(Date.now()).toLocaleTimeString('en-US');
+        const watchPage = window.location.href.includes('watch');
+        //console.debug("AdSkip - Video page? : window.location.href.includes('watch'));
         //console.debug(curTime + " - AdSkip: Observer Listening");
         for (let mutation of mutationsList) {
             if (mutation.type === 'childList') {
 
                 // Check if an ad is showing
                 const midVidAd = document.querySelector('.ad-showing');
-				const homePageIframe = document.getElementById('player');
-                //const homePagePremBanner = document.getElementById('masthead-ad');
-                const homePagePremBanner = document.querySelectorAll('[id=masthead-ad]');
-				const homePageInFeed = document.querySelectorAll('ytd-ad-slot-renderer');//.parentElement;
+                const midVidPaper = document.getElementById('tp-yt-paper-dialog');
+		const homePageMasthead = document.querySelectorAll('div#masthead-ad');
+                const homePageInFeed = document.querySelectorAll('ytd-rich-item-renderer');
+                const homePageInFeedTV = document.querySelector('ytd-primetime-promo-renderer');
+                const homePageInFeedPrem = document.querySelector('ytd-statement-banner-renderer');
                 //console.debug(homePageInFeed);
-                if (!midVidAd && !homePageIframe && !homePagePremBanner && !homePageInFeed) {
-                    State = "Listening";
-                    return;
-                }
 
                 if (midVidAd) { // Mid-video ad break //
                     console.log(curTime + " - AdSkip: An ad is currently Playing.");
                     const skipButton = document.querySelector('button.ytp-ad-skip-button.ytp-button');
                     if (skipButton) {
-                        skipButton.click();
-                        console.log(curTime + " - AdSkip: Button found and clicked.");
-                        State = "Listening"
-                        BlockedInterval = 10;
-                        setColor();
+                        State = "Listening";
+                        clickElement(skipButton, null, 'Button found and clicked.');
                     } else {
                         State = "Running";
                         BlockedInterval = 0;
@@ -55,44 +53,64 @@
                         //console.log(curTime + " - AdSkip: Skip button not ready.");
                     }
                 }
-				if (homePageIframe) { // Hopepage Video ad inside iframe //
-					const VideoAd = homePageIframe.contentDocument?.querySelector('#movie_player');
-				    if (VideoAd && VideoAd.classList.contains("playing-mode")) {
-						VideoAd.classList.remove("playing-mode");
-						VideoAd.classList.add("ytp-small-mode");
-						VideoAd.classList.add("unstarted-mode");
-                        console.log(curTime + " - AdSkip: Home Page video ad paused.");
-						BlockedInterval = 10;
-					}
-				}
-				/*if (homePagePremBanner) { // Homepage, Large banner ad for premium subscription //
-                    if (homePagePremBanner.style.display != 'none') {
-                        homePagePremBanner.style.display = "none";
-                        console.log(curTime + " - AdSkip: Home Page banner hidden.");
-                        BlockedInterval = 10;
-                    }
-				}*/
-                if (homePagePremBanner.length>0) {
-                    homePagePremBanner.forEach((banner) => {
-                        if (banner.style.display != 'none') {
-                            banner.style.display = 'none';
-                            console.log(curTime + " - AdSkip: Home Page banner hidden.");
-                            BlockedInterval = 10;
-                        }
+                if (homePageMasthead.length > 0) { // Top of feed large banner ad, with or without a video. //
+                    homePageMasthead.forEach(temp => removeElement(temp, "Home Page banner ad removed. (MastHead)"));
+                }
+                if (midVidPaper) { // Mid-Video Pop-up modal //
+                    clickElement(midVidPaper, '[id=dismiss-button] button', '"Paper Dialogue" pop-up blocked.');
+                }
+                if (homePageInFeed.length>0 && !watchPage) { // Homepage, Small in-feed ads that look like a video. //
+                    homePageInFeed.forEach(temp => {
+                        if (temp.querySelector('ytd-ad-slot-renderer')) { removeElement(temp, "Home Page in-feed ad removed."); }
                     });
                 }
-                if (homePageInFeed.length>0) {
-				    homePageInFeed.forEach((temp) => { // Homepage, Small in-feed ads that look like a video. //
-                        const inFeed = temp.parentElement.parentElement;
-                        if (inFeed.style.display != 'none') {
-                            inFeed.style.display = 'none';
-                            console.log(curTime + " - AdSkip: Home Page in-feed ad hidden.");
-                            BlockedInterval = 10;
-                        }
-                    });
+                if (homePageInFeed.length>0 && watchPage) { // Watch Page, in-feed ad that look like a video. //
+		    homePageInFeed.forEach((temp) => removeElement(temp, '"Next Video" ad removed.'));//hideElement(temp, '"Next Video" ad hidden.'));
+                }
+                if (homePageInFeedTV) { // Homepage, YouTubeTV mid-feed YTTV banner. //
+                    clickElement(homePageInFeedTV, 'button', "YouTubeTV ad banner hidden. (Primetime Promo)");
+                }
+                if (homePageInFeedPrem) { // Homepage, YouTubeTV mid-feed Premium banner. //
+                    clickElement(homePageInFeedPrem,'button',"YouTube Premium ad banner hidden. (Statement Banner)");
                 }
             }
         }
+    }
+
+    function clickElement(Element, descriptor, Msg) {
+        descriptor === null ? Element.click() : Element.querySelector(descriptor).click();
+        BlockedInterval = 10;
+        setColor();
+
+        if (Msg === null) { return; }
+        const curTime = new Date(Date.now()).toLocaleTimeString('en-US');
+        console.log(curTime + " - AdSkip: " + Msg);
+    }
+    function removeElement(Element, Msg) {
+        Element.remove();
+        const curTime = new Date(Date.now()).toLocaleTimeString('en-US');
+        console.log(curTime + " - AdSkip: " + Msg);
+    }
+    function hideElement(Element, Msg) {
+        if (Element.style.display === 'none') { return; }
+        Element.style.display = 'none';
+        BlockedInterval = 10;
+        setColor();
+
+        if (Msg === null) { return; }
+        const curTime = new Date(Date.now()).toLocaleTimeString('en-US');
+        console.log(curTime + " - AdSkip: " + Msg);
+    }
+    function DEBUG_highlightElement(Element) {
+        //Element.style.backgroundColor = '#FFFF00';
+        setTimeout(function() {
+            Element.style.transition = 'background-color 1s';
+            Element.style.backgroundColor = '#00FF00'; // Set to transparent to start the fade
+        }, 1);
+        Element.addEventListener('transitionend', function() {
+            Element.style.removeProperty('background-color');
+            Element.style.removeProperty('transition');
+        }, { once: true });
     }
 
     // Set Menu Bar color for status reporting.
@@ -114,8 +132,6 @@
                 menuBar.style.color = '#FFFF00';
                 menuBar.title = "AdSkip: Ad Detected. Trying to skip.";
                 break;
-            /* case "Success":
-                break; */
             default:
                 menuBar.style.color = '#666666';
                 menuBar.title = "AdSkip: Not running on current page. Sum Ting Wong";
@@ -129,4 +145,29 @@
     observer.observe(document.body, observerOptions);
 
     setInterval(setColor, 1000);
+
+    //Debug button:
+    const debugButton = document.createElement('div');
+    const searchBar = document.querySelector('ytd-searchbox[id="search"] div[id="container"]');
+
+    debugButton.innerHTML = `
+        <div>
+            <svg width="25px" height="25px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="currentColor">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M4 2H3V14H4V2ZM7.29062 2.59314L6.5 3.00001V13L7.29062 13.4069L14.2906 8.40687V7.59314L7.29062 2.59314ZM13.1398 8.00001L7.5 12.0284V3.9716L13.1398 8.00001Z"/>
+            </svg>
+        </div>
+    `;
+
+    if (searchBar && debugMode) {
+        searchBar.appendChild(debugButton);
+    }
+    // Add a click event to the debug button
+    debugButton.addEventListener('click', function() {
+        const searchBarText = document.querySelector('input#search').value;
+        const Elements = document.querySelectorAll(searchBarText)
+        Elements.forEach(temp => {
+            console.debug(temp);
+            DEBUG_highlightElement(temp);
+        });
+    });
 })();
